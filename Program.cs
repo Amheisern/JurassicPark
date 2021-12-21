658ac7f6dcaef03bff9372fd5aa05a03c35e8db6
lodashreload <icarusjapan@gmail.com>
2021-12-21 15:55:57 -0500

    added dino class and database class

--- Program.cs
+++ Program.cs
@@ -4,11 +4,90 @@ using System.Linq;
 
 namespace JurassicPark
 {
+    class DinosDatabase
+    {
+        private List<Dino> Dinos { get; set; } = new List<Dino>();
+
+        public void FindDinos(string dinoName)
+        {
+            Dino foundDino = Dinos.FirstOrDefault(dino => dino.name.ToUpper().Contains(dinoName.ToUpper()));
+        }
+        public void RemoveDinos(Dino dinoToDelete)
+        {
+            Dinos.Remove(dinoToDelete);
+        }
+        //Adding a Dino to DinosDatabase
+        public void AddDinos(Dino newDino)
+        {
+            Dino.add(newDino);
+        }
+        // public void DisplayDinos(Dino ShowDino)
+        // {
+
+        // }
+    }
+    class Dinos
+    {
+        public string Name { get; set; }
+        public string Diet { get; set; }
+        public int weight { get; set; }
+        public int Enclosure { get; set; }
+        public DateTime Date { get; set; } // Need help with this
+    }
+
     class Program
     {
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("    Welcome to JurassicPark D.R.A.F.T.S database   ");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
+
+        //^^^^^^^^^^^^^^^^^^^^^^^ methods, classes, oh my ^^^^^^^^^
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C#");
+            // our dino database
+            var database = new DinosDatabase();
+
+            // Should we keep showing out menu
+            var keepGoing = true;
+
+            DisplayGreeting();
+
+            //While user hasn't input "Q" keep showing menu
+            while (keepGoing)
+            {
+                Console.WriteLine("test");
+            }
         }
     }
 }
+
